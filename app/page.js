1178b94de75023692e7c18565340aa9da08e5990
Piotr <piotr_as@yahoo.com>
2023-11-25 10:05:15 +0100

    HomePage beginning

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
 "use client";
-import { useState } from 'react'
+import { useState } from "react";
 import Image from "next/image";
 import { Header } from "./components/Header/Header";
 import styles from "./Styles/HomePage.module.css";
-import Link  from "next/link";
+import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa";
 
 export default function Home() {
-  const [isHovered, setIsHovered ] = useState(false)
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -24,7 +24,7 @@ export default function Home() {
         className={`${styles.HomePage}
                          min-w-screen min-h-screen
                          relative overflow-hidden
-                         px-[10%] bg-gray-200
+                         pl-[10%] bg-gray-200
       `}
       >
         <div
@@ -58,14 +58,14 @@ export default function Home() {
         </div>
         <section
           className="
-                     flex justify-between
+                     flex
                      h-full w-full z-10
                     
         "
         >
           <div
             className="HomePage__describe
-                       w-[50%] h-100 flex flex-col
+                       w-[50%] h-full flex flex-col
           "
           >
             <h1
@@ -88,60 +88,36 @@ export default function Home() {
             </p>
             <Link
               href="/AboutPage"
-              className={`readMoreBtn uppercase my-8 font-extrabold text-2xl flex items-center ${isHovered ? 'jello' : ''}`}
+              className={`readMoreBtn uppercase my-8 
+              font-extrabold text-2xl flex items-center z-30 ${
+                isHovered ? "jello" : ""
+              }`}
               onMouseEnter={handleMouseEnter}
               onMouseLeave={handleMouseLeave}
             >
-              <span>read more</span>{' '} <FaArrowRight 
+              <span>read more</span>{" "}
+              <FaArrowRight
                 className={`${styles.ArrowRight} ml-4 inline-block `}
               />
             </Link>
-            <div className="about-box w-full mt-10">
-              <Link
-                href='#'
-                className={`${styles.portfolioLink}
-                               flex justify-between items-center
-                               py-1 cursor-pointer
-                               uppercase w-full 
-              `}
-              >
-                <span className="font-bold italic">#01</span>
-                <span className="font-bold">portfolio</span>
-              </Link>
-              <Link
-                href='#'
-                className={`${styles.resumeLink} 
-                             flex justify-between align-center 
-                             py-1
-                             uppercase w-full cursor-pointer`}
-              >
-                <span className="font-bold italic">#02</span>
-                <span className="font-bold">resume</span>
-              </Link>
-            </div>
           </div>
+          {/* IMAGE CONTAINER */}
           <div
             className="HomePage__image
-                       w-[50%] h-100
-                       relative
+                       w-full h-screen
+                       relative bg-black-100
+                       
           "
           >
             <Image
-              className="ml-auto rounded-full"
-              src="/profilImgTransp.webp"
+              className="ml-auto"
+              src="/Ja3.png"
               alt="Piotr Słobiński"
-              width={500}
-              height={300}
+              width={0}
+              height={0}
+              sizes="100%"
+              style={{ width: "90%", height: "auto", objectFit: "contain" }}
             />
-            <h2
-              className={`${styles.header2} absolute uppercase
-                         font-extrabold
-                         right-[11%] bottom-[-22%]
-                         text-[17rem]
-            `}
-            >
-              dev
-            </h2>
           </div>
         </section>
       </main>
