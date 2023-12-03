6405ce233d83755f06b832e0ede5c303ae79a13e
Capoverde <piotr_as@yahoo.com>
2023-12-03 13:51:22 +0100

    NavLinks

--- app/components/NavLinks/NavLinks.jsx
+++ app/components/NavLinks/NavLinks.jsx
@@ -5,7 +5,9 @@ import { GoArrowDownRight } from 'react-icons/go';
 export const NavLinks = ({ href, number, title }) => (
   <Link href={href} className="flex justify-between border-t 
                     text-gray-400 text-xl
-                    border-r border-[#3e3e3e] px-3 py-1">
+                    border-r border-[#3e3e3e] px-3 py-1
+                    z-[50]
+                    ">
     <span className="inline-block mr-auto italic ">#{number}</span>
     <span className="pr-8 font-bold">{title}</span> <GoArrowDownRight className='text-2xl'/>
   </Link>
