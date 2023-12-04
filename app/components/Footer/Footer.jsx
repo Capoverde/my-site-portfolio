ebc792715918c4bf80832b27ecccd272e017e2d2
Piotr <piotr_as@yahoo.com>
2023-12-04 20:05:08 +0100

    some changes

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -9,12 +9,10 @@ export const Footer = () => {
       className="w-full py-3 flex flex-end
                      bg-black text-gray-600"
     >
-
         <span className="inline-block ml-auto">Copy Right <sup>&copy;</sup>  {year} </span>
         <Link className="inline-block ml-4 mr-[11%]" href="/">
           Slobinski.dev
         </Link>
-      
     </footer>
   );
 };
