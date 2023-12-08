68c9482ed01c8e6fb3e95c3a59cc5ebd5604c8dd
Piotr <piotr_as@yahoo.com>
2023-12-08 12:31:34 +0100

    PortfolioPage grid

--- app/PortfolioPage/page.jsx
+++ app/PortfolioPage/page.jsx
@@ -11,35 +11,47 @@ import {
 import { Header } from "../components/Header/Header";
 import { Footer } from '../components/Footer/Footer'
 import { FeaturesTitle } from "../components/FeaturesTitle";
+import { AnimatedHeader } from "../components/AnimatedText/AnimatedHeader";
+import { AnimatedP } from "../components/AnimatedText/AnimatedP";
 import styles from "./PortfolioPage.module.css";
+
 
 const features = [
   {
     title: "FullStack ToDo Applicaton",
+    describe: "application with coll features",
     id: "to-do-app",
     card: CardProductCatalog,
   },
   {
     title: "FullStack product catalog store",
     id: "product-catalog",
+    describe: "application with coll features",
     card: WbsitteNext,
   },
   {
     title: "Website in Next.js",
     id: "lawyers-ebsite",
+    describe: "application with coll features",
     card: EventHorizon,
   },
   {
     title: "The Event Horizon",
     id: "website and gallery",
+    describe: "application with coll features",
     card: TheMet,
   },
   {
     title: "The Met",
     id: "museum-website",
+    describe: "application with coll features",
     card: Interdom,
   },
 ];
+
+const portfolioText = `Checkout Portfolio Project by cliking in the project image on the right.
+                      Here You will find my selected projects in a various tech stacks. 
+ `
 
 const layoutVariants = {
   initial: { opacity: 0 },
@@ -49,56 +61,46 @@ const layoutVariants = {
 
 const PortfolioPage = () => {
   return (
-    // <div className={`${styles.fadeIn} w-screen h-screen puff-in-center`}>
-    //   <Header />
-    //   <header className="Portfolio__header bg-gray-200">
-    //     <h1 className="title font-bold text-[6rem] pt-[3rem] px-[10%]">
-    //       Portfolio
-    //     </h1>
-    //   </header>
-    //   <main className="min-w-screen min-h-screen bg-gray-200 px-[10%]">
-    //     <div className="flex w-full h-full gap-20 items-start">
-    //       <div className="w-full py-[50vh]">
-    //         <ul>
-    //           {features.map((feature) => (
-    //             <li key={feature.id}>
-    //               <FeaturesTitle id={feature.id}>{feature.title}</FeaturesTitle>
-    //             </li>
-    //           ))}
-    //         </ul>
-    //       </div>
-    //       <div className="w-full sticky top-0 flex h-screen items-center">
-    //         <div className="relative w-full aspect-square bg-gray-100 rounded-[25px]">
-    //           <AnimatePresence exitBeforeEnter={false} mode="wait">
-    //             <m.div
-    //               key={features.map((feature) => feature.id).join("-")}
-    //               initial="initial"
-    //               animate="animate"
-    //               exit="exit"
-    //               variants={layoutVariants}
-    //               transition={{ duration: 0.75 }}
-    //             >
-    //               {features.map((feature) => (
-    //                 <feature.card id={feature.id} key={feature.id} />
-    //               ))}
-    //             </m.div>
-    //           </AnimatePresence>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </main>
-    //   <Footer />              
-    // </div>
-    <div  className={`${styles.fadeIn} w-screen h-screen puff-in-center`}>
-    <Header />
-      <div className="wrapper mx-[10%] h-full border-l border-r border-[#3e3e3e]">
-        <header className={`${styles.title__header} w-full py-8 pl-6 border-b border-[#3e3e3e]`}>
-        <h1 className="title font-bold text-[6rem] text-gray-200">
-          Portfolio
-         </h1>
-        </header>
-        
-      </div>
+    <div className={`${styles.fadeIn} w-screen h-screen puff-in-center`}>
+      <Header />
+      <header className="Portfolio__header pl-6 mx-[10%] border-b border-l border-r border-[#3e3e3e] ">
+       <AnimatedHeader text="Portfolio" className="title font-bold text-[6rem] text-gray-200" />
+      </header>
+      <main className="min-w-screen min-h-screen mx-[10%] boder-l border-r border-[#3e3e3e]">
+        <div className="flex w-full h-full items-start border-l border-[#3e3e3e]">
+          <div className="w-full py-[50vh] relative">
+            <div className="h-[200px] w-full absolute top-0 text-gray-300 border-b border-[#3e3e3e]">
+             <AnimatedP text={portfolioText} className="py-4 text-gray-300 px-6" />
+            </div>
+            <ul className="px-6">
+              {features.map((feature) => (
+                <li key={feature.id}>
+                  <FeaturesTitle id={feature.id}>{feature.title}</FeaturesTitle>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="w-full sticky top-0 flex h-screen items-center border-l border-[#3e3e3e]">
+            <div className="relative w-full aspect-square bg-gray-100 rounded-[25px]">
+              <AnimatePresence exitBeforeEnter={false} mode="wait">
+                <m.div
+                  key={features.map((feature) => feature.id).join("-")}
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                  variants={layoutVariants}
+                  transition={{ duration: 0.75 }}
+                >
+                  {features.map((feature) => (
+                    <feature.card id={feature.id} key={feature.id} />
+                  ))}
+                </m.div>
+              </AnimatePresence>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer />              
     </div>
   );
 };
--- app/components/FeaturesTitle.jsx
+++ app/components/FeaturesTitle.jsx
@@ -4,7 +4,6 @@ import { useInView } from 'framer-motion'
 import classNames from 'classnames'
 import { useFeatureStore } from './store'
 
-
 export const FeaturesTitle = ({ children, id }) => {
     const ref = useRef(null)
     const isInView = useInView(ref, { margin: "-50% 0px -50% 0px" })
@@ -18,7 +17,7 @@ export const FeaturesTitle = ({ children, id }) => {
 
   return (
     <p ref={ref} className={classNames("text-5xl font-extrabold py-16 transition-colors",
-                                       isInView ? "text-black" : "text-gray-500" 
+                                       isInView ? "text-gray-200" : "text-gray-500" 
                             )}>
                 {children}
     </p>
