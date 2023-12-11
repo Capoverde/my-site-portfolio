a2db9102bcb89211def64869a6bb9f8e8978b0f5
Capoverde <piotr_as@yahoo.com>
2023-12-11 14:20:47 +0100

    Revert "working version"
    This reverts commit 2f8b70cd9817b569449b987f98a08da25bc1860a.

--- app/AboutPage/AboutContent.jsx
+++ app/AboutPage/AboutContent.jsx
@@ -1,20 +1,21 @@
 // AboutContent.jsx
 import React, { useContext, useState } from 'react';
 import { AnimatedP } from '../components/AnimatedText/AnimatedP';
-import SkillButtonContext from '../components/SkillButton/SkillButtonContext';
+import { useSkillButtonContext } from '../components/SkillButton/SkillButtonContext';
 
 export const AboutContent = () => {
-
- const context = useContext(SkillButtonContext);
- const { selectedData, updateSelectedData } = useSkillButtonContext();
+  const { selectedData } = useSkillButtonContext();
 
   return (
     <>
-     <AnimatedP
-      text={selectedData ? selectedData.describe : 'A diligent full stack developer with expertise in HTML, CSS, JavaScript/TypeScript, and React, I create intuitive and secure interfaces by combining my creative and analytical skills with robust security measures. My diverse background in programming, property management, and pilot training allows me to adapt to challenges, work closely with teams, and deliver high-quality web applications.'}
-      
-      className="text-gray-400 px-6 py-6 h-full"
-     />
+      <AnimatedP
+        text={
+          selectedData
+            ? selectedData.describe
+            : 'A diligent full stack developer with expertise in HTML, CSS, JavaScript/TypeScript, and React, I create intuitive and secure interfaces by combining my creative and analytical skills with robust security measures. My diverse background in programming, property management, and pilot training allows me to adapt to challenges, work closely with teams, and deliver high-quality web applications.'
+        }
+        className="text-gray-400 px-6 py-6 h-full"
+      />
     </>
   );
 };
--- app/components/SkillButton/SkillButton.jsx
+++ app/components/SkillButton/SkillButton.jsx
@@ -1,25 +1,18 @@
 import React, { useState } from 'react';
-import { motion as m } from 'framer-motion';
+import { motion } from 'framer-motion';
 import styles from './SkillButton.module.css';
 import technoData from '../../AboutPage/technoData.json';
 
-export const SkillButton = ({ id, children }) => {
-  const { updateSelectedData } = useSkillButtonContext();
+export const SkillButton = ({  children }) => (
+  
+    <motion.button
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      className=" px-4 py-1 flex items-center align-center
+                                     cursor-pointer
+                                     border rounded-3xl border-gray-800"
+    >
+     <span className={`${styles.skillBtn}`}>{children}</span>
+    </motion.button>
 
-  const handleClick = () => {
-    const data = technoData.find((item) => item.id === id);
-    updateSelectedData(data);
-  };
-
-  return (
-    <button>
-      <m.button
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        className="px-4 py-1 flex items-center align-center cursor-pointer border rounded-3xl border-gray-800"
-      >
-        <span className={`${styles.skillBtn}`}>{children}</span>
-      </m.button>
-    </button>
-  );
-};
+);
